c1474fd44ef5e103c253c7c1deeb6881a168a378
chrindex <7467271+chrindex@user.noreply.gitee.com>
2023-08-10 12:50:15 +0800

    去除无用打印，添加一个std::thread的threadpool

--- src/base/threadpool.cpp
+++ src/base/threadpool.cpp
@@ -46,7 +46,7 @@ namespace chrindex::andren::base
                         else if(data->isExit) 
                         {
                             /// 当且仅当任务为空时才判断要不要退出
-                            printf("Thread %d Will Be Exit.\n",threadno);
+                            //printf("Thread %d Will Be Exit.\n",threadno);
                             return;
                         }
                         else 
@@ -87,6 +87,77 @@ namespace chrindex::andren::base
     }
 
     ThreadPool::perthread_data_t::~perthread_data_t() 
+    { 
+        cond.notify_one(); 
+    }
+
+    /// ############## ThreadPoolPortable  Using std::thread ###################
+
+    ThreadPoolPortable::ThreadPoolPortable(uint32_t threadCount)
+    {
+        m_data = std::make_shared<data_t>(threadCount);
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            int ret = 0;
+            std::thread([data = m_data, threadno = i] /*注意保活*/
+                     {
+                    // 不要把锁提到for里面
+                    std::unique_lock<std::mutex> locker(data->perthread_data[threadno].mut);
+                    for(;;)
+                    {
+                        if( ! data->perthread_data[threadno].tasks.empty()) 
+                        {
+                            /// 任务空时自然不用退出
+                            auto tasks = std::move(data->perthread_data[threadno].tasks);
+                            // 在此处解锁让外部线程add task
+                            locker.unlock();
+                            while(tasks.size()>0){
+                                tasks.front()();
+                                tasks.pop();
+                            }
+                            locker.lock();
+                        }
+                        else if(data->isExit) 
+                        {
+                            /// 当且仅当任务为空时才判断要不要退出
+                            return;
+                        }
+                        else 
+                        {
+                            data->perthread_data[threadno].cond.wait_for(locker,std::chrono::milliseconds(10));
+                        }
+                    } }).detach();
+        }
+    }
+
+    ThreadPoolPortable::~ThreadPoolPortable()
+    {
+        if (m_data){
+            m_data->isExit = true;
+        }
+    }
+
+    ThreadPoolPortable &ThreadPoolPortable::operator=(ThreadPoolPortable &&_)
+    {
+        this->~ThreadPoolPortable();
+        m_data = std::move(_.m_data);
+        return *this;
+    }
+
+    ThreadPoolPortable::data_t::data_t(uint32_t _thread_count)
+    {
+        isExit = false;
+        perthread_data = new perthread_data_t[_thread_count];
+        thread_count = _thread_count;
+    }
+    ThreadPoolPortable::data_t::~data_t()
+    {
+        isExit = true;
+        delete[] perthread_data;
+    }
+
+    ThreadPoolPortable::perthread_data_t::~perthread_data_t() 
     { 
         cond.notify_one(); 
     }
--- src/base/threadpool.hh
+++ src/base/threadpool.hh
@@ -19,6 +19,7 @@ namespace chrindex::andren::base
     uint32_t hardware_vcore_count();
 
     /// @brief 简单的线程池
+    /// 使用pthread的包装Thread
     class ThreadPool
     {
     public:
@@ -70,4 +71,60 @@ namespace chrindex::andren::base
 
         std::shared_ptr<data_t> m_data;
     };
+
+    
+    /// @brief 简易线程池
+    /// 使用pthread的标准库包装std::thread 
+    class ThreadPoolPortable
+    {
+    public:
+        ThreadPoolPortable() = default;
+
+        ThreadPoolPortable(ThreadPoolPortable &&) = default;
+
+        ThreadPoolPortable(uint32_t threadCount);
+
+        ~ThreadPoolPortable();
+
+        ThreadPoolPortable & operator=(ThreadPoolPortable && _);
+
+        template <typename T>
+        auto exec(T &&task , uint32_t threadno) -> bool
+        {
+            if (!m_data && threadno >= m_data->thread_count)
+            {
+                return false;
+            }
+            SCOPE_RUN(std::lock_guard, m_data->perthread_data[threadno].mut,
+                      {
+                          m_data->perthread_data[threadno].tasks.emplace(std::forward<T>(task));
+                      });
+            m_data->perthread_data[threadno].cond.notify_one();
+            return true;
+        }
+
+    private:
+
+        struct perthread_data_t
+        {
+            std::mutex mut;
+            std::condition_variable cond;
+            std::queue<std::function<void()>> tasks;
+
+            perthread_data_t()= default;
+            ~perthread_data_t();
+        };
+
+        struct data_t
+        {
+            data_t(uint32_t _thread_count);
+            ~data_t();
+            std::atomic<bool> isExit;
+            perthread_data_t* perthread_data;
+            uint32_t thread_count;
+        };
+
+        std::shared_ptr<data_t> m_data;
+    };
+
 }
