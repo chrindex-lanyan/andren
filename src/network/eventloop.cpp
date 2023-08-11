f47bd3c3d5e68d755bcc906a4844d178464ef30d
chrindex <7467271+chrindex@user.noreply.gitee.com>
2023-08-11 12:58:56 +0800

    eventloop 初版

---
+++ src/network/eventloop.cpp
@@ -0,0 +1,122 @@
+﻿
+
+#include "eventloop.hh"
+
+#include <algorithm>
+
+
+namespace chrindex::andren::network
+{
+    EventLoop::EventLoop(uint32_t size)
+    {
+        m_size = std::max(size, 2u);
+        m_bqueForTask = new base::DBuffer<Task>[m_size];
+        m_exit = false;
+    }
+
+    EventLoop::~EventLoop()
+    {
+        shutdown();
+        delete [] m_bqueForTask;
+    }
+
+    bool EventLoop::addTask(Task task, EventLoopTaskType type)
+    {
+        if(!m_tpool.valid() || !task)
+        {
+            return false;
+        }
+
+        uint64_t randmsec = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock()
+            .now().time_since_epoch()).count();
+        int32_t randindex = randmsec%(m_size-1);
+        if (type == EventLoopTaskType::IO_TASK)
+        {
+            m_bqueForTask[0].pushBack(std::move(task));
+        }
+        else if (type == EventLoopTaskType::SHCEDULE_TASK)
+        {
+            m_bqueForTask[1].pushBack(std::move(task));
+        }
+        else if (type == EventLoopTaskType::FURTURE_TASK)
+        {
+            m_bqueForTask[randindex].pushBack(std::move(task));
+        }
+
+        return true;
+    }
+
+    bool EventLoop::addTask(Task task, uint32_t index)
+    {
+        if(!m_tpool.valid() || !task)
+        {
+            return false;
+        }
+        if (index >= m_size)
+        {
+            index = m_size -1;
+        }
+
+        m_bqueForTask[index].pushBack(std::move(task));
+        return true;
+    }
+
+    bool EventLoop::start()
+    {
+        m_tpool = std::move(base::ThreadPoolPortable(m_size));
+        for (uint32_t i=0;i< m_size;i++)
+        {
+            startNextLoop(i);
+        }
+        return true;
+    }
+
+    void EventLoop::shutdown()
+    {
+        // 设置退出标识
+        m_exit = true;
+        // 唤醒所有的线程
+        for (uint32_t i = 0; i < m_size ; i++)
+        {
+            m_tpool.notifyThread(i);
+        }
+    }
+
+    bool EventLoop::isShutdown()const 
+    {
+        return m_exit;
+    }
+
+    void EventLoop::startNextLoop(uint32_t index)
+    {
+        if (m_exit)
+        {
+            return ;
+        }
+        m_tpool.exec([self = shared_from_this(), index]()->bool
+        {
+            self->work(index);
+            self->startNextLoop(index);
+            return true;
+        },index);
+    }
+
+    void EventLoop::work(uint32_t index)
+    {
+        std::optional<std::deque<Task>> result;
+        m_bqueForTask[index].takeMulti(result);
+
+        if (!result.has_value())
+        {
+            std::this_thread::sleep_for(std::chrono::microseconds(100));
+            return ;
+        }
+        for (auto & task : result.value() )
+        {
+            if (task)
+            {
+                task();
+            }
+        }
+    }
+}
---
+++ src/network/eventloop.hh
@@ -0,0 +1,71 @@
+﻿
+#pragma once
+
+
+#include "../base/andren_base.hh"
+
+namespace chrindex::andren::network
+{
+    enum EventLoopTaskType:int
+    {
+        SHCEDULE_TASK,  // 定时器任务（线程1~n）
+        IO_TASK,        // io任务（线程1）
+        FURTURE_TASK,   // 常规任务（线程1~n）
+    };
+    
+    class EventLoop : public std::enable_shared_from_this<EventLoop> , base::noncopyable
+    {
+    public :
+
+        using Task = std::function<void()>;
+
+        EventLoop(uint32_t size = std::thread::hardware_concurrency());
+        ~EventLoop();
+
+        /// @brief 添加任务
+        /// @param task 任务函数
+        /// @param type 任务类型
+        /// @return 添加成功与否
+        bool addTask(Task task , EventLoopTaskType type);
+
+        /// @brief 添加任务
+        /// @param task 任务函数
+        /// @param index 指派的线程。线程索引从0开始，不超过N。
+        /// @return 添加成功与否
+        bool addTask(Task task , uint32_t index);
+
+        /// @brief 启动事件循环
+        /// 注意，该函数是非阻塞的。
+        /// 不包含本线程在内的线程，数量共N个。
+        /// @return 返回true为成功
+        bool start();
+
+        /// @brief 关闭。
+        void shutdown();
+
+        /// @brief 是否停止或者即将停止了
+        /// @return 
+        bool isShutdown()const ;
+
+    private :
+
+        /// @brief 开始下一次的事件循环
+        void startNextLoop(uint32_t index);
+
+        /// @brief 工作线程函数
+        /// @param index 线程号
+        void work(uint32_t index);
+
+    private :
+        uint32_t m_size;
+        base::ThreadPoolPortable m_tpool;
+        base::DBuffer<Task>* m_bqueForTask;
+        std::atomic<bool> m_exit;
+    };
+
+
+}
+
+
+
+
