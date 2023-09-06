95381258574a27f85b4b709200c23b8a387ea180
chrindex <7467271+chrindex@user.noreply.gitee.com>
2023-09-07 02:42:19 +0800

    新建

---
+++ src/network/http1.1.hh
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+
+#include "../base/andren_base.hh"
+
+#include "../../third-part/llhttp_src/generate_src/llhttp.h"
+#include "../../third-part/llhttp_src/generate_src/api.h"
+
+namespace chrindex::andren::network
+{
+    class HTTP11_Request
+    {
+
+    };
+
+    class HTTP11_Response
+    {
+
+    };
+
+    class HTTP11_Session
+    {
+
+    };
+
+}
