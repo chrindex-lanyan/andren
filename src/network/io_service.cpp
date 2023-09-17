d0b52f1c5f8a6dbd17820b519111b25c3b73a97c
chrindex <7467271+chrindex@user.noreply.gitee.com>
2023-09-18 01:33:25 +0800

    io_service尝试性实践

---
+++ src/network/io_service.cpp
@@ -0,0 +1,40 @@
+﻿#include "io_service.hh"
+#include "events_service.hh"
+
+
+#include <liburing.h>
+
+
+namespace chrindex::andren::network
+{
+
+    IOService::IOService ()
+    {
+        m_uring =0;
+    }
+    IOService::IOService (IOService && ios) noexcept : EventsService(std::move(ios))
+    {
+        m_fds_context = std::move(ios.m_fds_context);
+        m_uring = std::move(ios.m_uring);
+        ios.m_uring = 0;
+    }
+
+    IOService::~IOService()
+    {
+        if (m_uring>0)
+        {
+            
+        }
+    }
+
+    void IOService::operator= (IOService && ios) noexcept
+    {
+        this->~IOService();
+        EventsService::operator=(std::move(ios));
+        m_fds_context = std::move(ios.m_fds_context);
+        m_uring = std::move(ios.m_uring);
+        ios.m_uring = 0;
+    }
+
+}
+
---
+++ src/network/io_service.hh
@@ -0,0 +1,26 @@
+﻿#pragma once 
+
+#include "../base/andren_base.hh"
+#include "events_service.hh"
+#include <functional>
+
+
+namespace chrindex::andren::network
+{
+
+    class IOService : public EventsService ,base::noncopyable
+    {
+    public :
+
+        IOService ();
+        IOService (IOService && ios) noexcept;
+        ~IOService();
+
+        void operator= (IOService && ios) noexcept;
+
+    private :
+        std::map<int , std::any> m_fds_context;
+        int m_uring;
+    };
+
+}
