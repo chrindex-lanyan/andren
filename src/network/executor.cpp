957902f117fef50755ddce747303c9061a15a972
chrindex <7467271+chrindex@user.noreply.gitee.com>
2023-09-17 12:17:22 +0800

    添加自动分配执行线程的执行器，以及一个简单分配器

---
+++ src/network/executor.cpp
@@ -0,0 +1,73 @@
+﻿#include "executor.hh"
+#include "task_distributor.hh"
+#include <memory>
+
+
+namespace chrindex::andren::network
+{
+    Executor::Executor():
+        m_schedule(0),m_ev(nullptr)
+    {
+        //
+    }
+
+    Executor::Executor(uint32_t nthreads) : 
+        m_schedule(nthreads),
+        m_ev(std::make_unique<TaskDistributor>(nthreads))
+    {
+        m_ev->start();
+    }
+
+    Executor::Executor(Executor && e) noexcept
+    {
+        m_schedule = std::move(e.m_schedule);
+        m_ev = std::move(e.m_ev);
+    }
+
+    Executor::~Executor()
+    {
+        m_ev->shutdown();
+    }
+
+    bool Executor::valid()const 
+    {
+        return m_ev && !m_ev->isShutdown();
+    }
+
+    bool Executor::addTask(int key , std::function<void(Executor *)> task)
+    {
+        return m_ev && m_ev->addTask([this, t = std::move(task)]()
+        {
+            t(this);
+        }, m_schedule.doSchedule(0UL + key));
+    }
+
+    bool Executor::addTask(std::string const & key , std::function<void(Executor *)> task)
+    {
+        return m_ev && m_ev->addTask([this, t = std::move(task)]()
+        {
+            t(this);
+        }, m_schedule.doSchedule(key));
+    }
+
+    bool Executor::addTaskRandom(std::function<void(Executor *)> task)
+    {
+        return m_ev && m_ev->addTask([this, t = std::move(task)]()
+        {
+            t(this);
+        }, m_schedule.doSchedule(reinterpret_cast<uint64_t>(this)));
+    }
+
+    void Executor::operator=(Executor && e)
+    {
+        this->~Executor();
+        m_schedule = std::move(e.m_schedule);
+        m_ev = std::move(e.m_ev);
+    }
+
+    bool Executor::operator==(Executor & e) const
+    {
+        return this == &e;
+    }
+
+}
---
+++ src/network/executor.hh
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "../base/andren_base.hh"
+
+#include "task_distributor.hh"
+
+#include "schedule.hh"
+#include <functional>
+#include <memory>
+
+
+namespace chrindex::andren::network
+{
+    class Executor : public base::noncopyable
+    {
+    public :
+        Executor();
+        Executor(uint32_t nthreads);
+        Executor(Executor &&) noexcept;
+        ~Executor();
+
+        bool valid()const ;
+
+        bool addTask(int key , std::function<void(Executor *)> task);
+
+        bool addTask(std::string const & key , std::function<void(Executor *)> task);
+
+        bool addTaskRandom(std::function<void(Executor *)> task);
+
+        void operator=(Executor &&);
+
+        bool operator==(Executor &) const;
+
+    private :
+        Schedule m_schedule;
+        std::unique_ptr<TaskDistributor> m_ev;
+
+    };
+
+
+}
