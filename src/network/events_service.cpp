fea41b92641df4339132818ccf8f6e90dfe308f8
chrindex <7467271+chrindex@user.noreply.gitee.com>
2023-09-18 01:32:24 +0800

    用于EventLoop的事件服务

---
+++ src/network/events_service.cpp
@@ -0,0 +1,67 @@
+﻿#include "events_service.hh"
+
+namespace chrindex::andren::network
+{
+    EventsService::EventsService()
+    {
+        m_key =-1;
+    }
+
+    EventsService::EventsService(int64_t key)
+    {
+        m_key = key;
+    }
+
+    EventsService::EventsService(EventsService && es) noexcept
+    {
+        m_key = std::move(es.m_key);
+        m_processor = std::move(es.m_processor);
+        m_handler = std::move(es.m_handler);
+    }  
+
+    EventsService::~EventsService()
+    {
+
+    }
+
+    void EventsService::operator =(EventsService && es) noexcept
+    {
+        this->~EventsService();
+        m_key = std::move(es.m_key);
+        m_processor = std::move(es.m_processor);
+        m_handler = std::move(es.m_handler);
+    }
+
+
+    void EventsService::setNotifier(Processor && notifier)
+    {
+        m_processor = std::move(notifier);
+    }
+
+    void EventsService::setEventsHandler(EventsHandler && handler)
+    {
+        m_handler = std::move(handler);
+    }
+
+    void EventsService::processEvents()
+    {
+        if (m_processor)[[likely]]
+        {
+            std::vector<base::KVPair<int64_t, int>> result;
+            m_processor(result);
+            if (m_handler)[[likely]]
+            {
+                for (auto const & kv : result)
+                {
+                    m_handler(kv.key(),kv.value_from_copy());
+                }
+            }
+        }
+    }
+
+    int64_t EventsService::getKey () const
+    {
+        return m_key;
+    }
+
+}
---
+++ src/network/events_service.hh
@@ -0,0 +1,41 @@
+﻿#pragma once 
+
+#include "../base/andren_base.hh"
+#include <cstdint>
+#include <functional>
+
+
+namespace chrindex::andren::network
+{
+    class EventsService
+    {
+    public :
+        EventsService();
+        EventsService(int64_t key);
+        EventsService(EventsService && ) noexcept;
+        virtual ~EventsService();
+
+        void operator =(EventsService &&) noexcept;
+
+        using Processor = std::function<void (std::vector<base::KVPair<int64_t, int>> & )>;
+        using EventsHandler = std::function<void (int64_t key , int events)>;
+
+        void processEvents();
+
+        int64_t getKey () const;
+    
+    protected:
+    
+        void setNotifier(Processor && processor);
+        void setEventsHandler(EventsHandler && handler);
+
+    private :
+
+        int64_t m_key;
+        Processor m_processor;
+        EventsHandler m_handler;
+
+    };
+    
+
+}
