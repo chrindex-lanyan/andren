43aa5f58c2606e6ed140fce07cd7ae179b51f79f
chrindex <7467271+chrindex@user.noreply.gitee.com>
2023-09-20 00:38:14 +0800

    完成初版io_service

--- src/network/io_service.cpp
+++ src/network/io_service.cpp
@@ -2,29 +2,30 @@
 #include "events_service.hh"
 
 
+#include <chrono>
 #include <liburing.h>
+#include <memory>
+#include <sys/socket.h>
+#include <utility>
 
 
 namespace chrindex::andren::network
 {
+    static inline constexpr auto DEFAULT_QUEUE_SIZE = 128;
 
-    IOService::IOService ()
+    IOService::IOService (int64_t key) : EventsService(key)
     {
-        m_uring =0;
+        init_a_new_io_uring(0);
     }
     IOService::IOService (IOService && ios) noexcept : EventsService(std::move(ios))
     {
         m_fds_context = std::move(ios.m_fds_context);
         m_uring = std::move(ios.m_uring);
-        ios.m_uring = 0;
     }
 
     IOService::~IOService()
     {
-        if (m_uring>0)
-        {
-            
-        }
+        deinit_io_uring();
     }
 
     void IOService::operator= (IOService && ios) noexcept
@@ -33,7 +34,192 @@ namespace chrindex::andren::network
         EventsService::operator=(std::move(ios));
         m_fds_context = std::move(ios.m_fds_context);
         m_uring = std::move(ios.m_uring);
-        ios.m_uring = 0;
+    }
+
+    bool IOService::submitRequest(int uid, io_context && _context)
+    {
+        auto & context = m_fds_context[uid];
+
+        context = std::move(_context);
+
+        if ( context.req_context->general.req == io_request::OTHER 
+        || context.req_context->general.req == io_request::HOSTING 
+        || context.req_context->general.req == io_request::DISCONNECT)
+        {
+            return false;
+        }
+
+        io_uring * puring = nullptr;
+        auto sqe = find_empty_sqe(&puring);
+        sqe->user_data = reinterpret_cast<uint64_t>(&context);
+
+        switch (context.req_context->general.req) 
+        {
+        case io_request::OPEN:
+        {
+            // fd 在 CQE 的 res 字段返回
+            io_uring_prep_openat(sqe, 
+                context.req_context->ioData.file.dfd, 
+                context.req_context->ioData.file.path, 
+                context.req_context->general.flags, 
+                context.req_context->ioData.file.mode);
+            break;
+        }
+        case io_request::CONNECT:
+        {
+            // ret 在 CQE 的 res 字段
+            io_uring_prep_connect(sqe, 
+                context.req_context->general.fd, 
+                reinterpret_cast<sockaddr*>(&context.req_context->ioData.addr.ss),
+                context.req_context->general.size);
+            break;
+        }
+        case io_request::ACCEPT:
+        {
+            io_uring_prep_accept(sqe, 
+                context.req_context->general.fd, 
+                reinterpret_cast<sockaddr*>(&context.req_context->ioData.addr.ss),
+                &context.req_context->general.size ,
+                context.req_context->general.flags);
+            break;
+        }
+        case io_request::READ:
+        {
+            io_uring_prep_recv(sqe, 
+                context.req_context->general.fd, 
+                context.req_context->ioData.bufData.buf , 
+                sizeof(context.req_context->ioData.bufData.buf),
+                context.req_context->general.flags);
+            break;
+        }
+        case io_request::WRITE:
+        {
+            io_uring_prep_send(sqe, 
+                context.req_context->general.fd, 
+                context.req_context->ioData.bufData.buf, 
+                sizeof(context.req_context->ioData.bufData.buf), 
+                context.req_context->general.flags);
+            break;
+        }
+        case io_request::CLOSE:
+        {
+            io_uring_prep_close(sqe, context.req_context->general.fd);
+            break;
+        }
+        default: // ignore
+        {
+            return false;
+        }
+        }
+        return true;
+    }
+
+    io_uring_sqe * IOService::find_empty_sqe(io_uring ** ppuring)
+    {
+        io_uring_sqe * ptr = nullptr;
+        int count = m_uring.size();
+
+        for (int i =0 ; i < count ;i++)
+        {
+            if(auto result = m_uring.min_pair() ;
+               result.has_value() && result->key() < DEFAULT_QUEUE_SIZE)
+            {
+                if (auto p = io_uring_get_sqe(result->value().get()) 
+                    ; p != nullptr)
+                {
+                    ptr = p;
+                    *ppuring = result->value().get();
+                    result = m_uring.extract_min_pair();
+                    m_uring.push( {result->key() + 1 , result->value()});
+                    break;
+                }
+            }
+        }
+
+        if (ptr == nullptr)
+        {
+            auto puring = init_a_new_io_uring(1);
+            ptr = io_uring_get_sqe(puring);
+        }
+
+        return ptr;
+    }
+
+    io_uring * IOService::init_a_new_io_uring(int used)
+    {
+        auto puring = std::make_shared<io_uring>();
+        m_uring.push( {used, puring });
+        io_uring_queue_init(DEFAULT_QUEUE_SIZE, puring.get(), 0);
+        return puring.get();
+    }
+
+    void IOService::deinit_io_uring()
+    {
+        m_uring.foreach_pair([](auto & pair)
+        {
+            if (pair.value())
+            {
+                auto result = pair.value().get();
+                io_uring_queue_exit(result);
+            }
+        });
+    }
+
+    void IOService::init()
+    {
+        setNotifier([this](std::vector<base::KVPair<int64_t, int>> & events [[maybe_unused]])
+        {
+            auto tmp_uring = std::move(m_uring);
+            tmp_uring.foreach_pair([&events, this](base::KVPair<int, std::shared_ptr<io_uring>> & pair)
+            {
+                auto submit_count = pair.key();
+                auto puring = pair.value().get();
+                int ret = io_uring_submit(puring);
+                if (ret < 0)
+                {
+                    /// error
+                }
+                struct io_uring_cqe * pcqe = 0;
+                struct __kernel_timespec kspec;
+
+                kspec.tv_nsec =  std::min(100 , submit_count * 1);
+                kspec.tv_sec = 0;
+
+                ret = io_uring_wait_cqes(puring, &pcqe, submit_count, &kspec, 0);
+
+                if (ret !=0)
+                {
+                    /// error
+                }
+                uint32_t head = 0;
+                int count =0;
+                io_uring_for_each_cqe(puring,head,pcqe)
+                {
+                    int64_t ioctx_ptr = 0;
+                    ::memcpy(&ioctx_ptr, &pcqe->user_data ,sizeof(uint64_t)) ;
+                    events.push_back({ ioctx_ptr, pcqe->res});
+                    count++;
+                }
+                io_uring_cq_advance(puring, count);
+
+                /// 更新剩余提交数后放回uring结构
+                m_uring.push({submit_count - count , std::move(pair.value())});
+            });
+
+        });
+
+        setEventsHandler([this](int64_t key [[maybe_unused]] , int ev [[maybe_unused]])
+        {
+            auto ioctx_ptr = reinterpret_cast<io_context *>(key);
+            auto iter = m_fds_context.find(ioctx_ptr->req_context->general.uid);
+            auto _tmp = std::move(iter->second); 
+            m_fds_context.erase(iter);  
+            if(ioctx_ptr->onEvents)
+            {
+                ioctx_ptr->onEvents(ioctx_ptr);
+            }
+        });
+
     }
 
 }
--- src/network/io_service.hh
+++ src/network/io_service.hh
@@ -2,25 +2,114 @@
 
 #include "../base/andren_base.hh"
 #include "events_service.hh"
+#include <deque>
 #include <functional>
+#include <future>
+#include <liburing.h>
+#include <memory>
+#include <sys/socket.h>
+
 
 
 namespace chrindex::andren::network
 {
+    /// io request structure
+    /// sizeof 1024 
+    struct io_request
+    {
+        enum REQ { OPEN, CONNECT, ACCEPT, 
+            READ, WRITE, DISCONNECT, CLOSE, HOSTING, OTHER };
+
+        struct general_t
+        {
+            REQ _IN_OUT_ req;
+            int64_t uid;
+            uint32_t _IN_OUT_ size; // socklen / path len / buf len 
+            int32_t _IN_ flags; // for : file / socket
+            int32_t _IN_OUT_ fd;// for : file / socket
+        } general;
+
+        union {
+            struct addr_t{
+                sockaddr_storage _OUT_ ss;
+            } addr;// for : accept
+
+            struct file_t{
+                int32_t _IN_ dfd;
+                uint32_t _IN_ mode;
+                char _IN_ path[256]; 
+            } file;// for : open
+
+            struct buf_data_t
+            {
+#define _OTHER_STRUCTURE_SIZE_ (std::max(sizeof(file_t), sizeof(addr_t)))
+                // for other structure
+                char _IN_OUT_ _others_structure[_OTHER_STRUCTURE_SIZE_];
+
+#define _BUF_SIZE_ (1024 - ( sizeof(general) + sizeof(_others_structure)))
+                // for : read , write
+                char _IN_OUT_ buf[_BUF_SIZE_]; 
+            } bufData;
+        } _IN_OUT_ ioData ;
+    };
+
+    struct io_context
+    {
+        io_context() : req_real(sizeof(io_request),0)
+        {
+            req_context = reinterpret_cast<io_request*>(&req_real[0]);
+        }
+        DEFAULT_DECONSTRUCTION(io_context);
+        
+        io_context(io_context const & other) 
+        {
+            onEvents = other.onEvents;
+            req_real = other.req_real;
+            req_context = reinterpret_cast<io_request*>(&req_real[0]);
+        }
+        
+        void operator=(io_context const & other)
+        {
+            onEvents = other.onEvents;
+            req_real = other.req_real;
+            req_context = reinterpret_cast<io_request*>(&req_real[0]);
+        }
+        
+        DEFAULT_MOVE_CONSTRUCTION(io_context);
+        DEFAULT_MOVE_OPERATOR(io_context);
+        
+        std::function<void(io_context *)> onEvents;
+        std::string req_real;
+        io_request* req_context;
+    };
 
     class IOService : public EventsService ,base::noncopyable
     {
     public :
 
-        IOService ();
+        IOService (int64_t key);
         IOService (IOService && ios) noexcept;
         ~IOService();
 
         void operator= (IOService && ios) noexcept;
 
+        bool submitRequest(int uid, io_context && context);
+
     private :
-        std::map<int , std::any> m_fds_context;
-        int m_uring;
+
+        io_uring * init_a_new_io_uring(int used);
+
+        io_uring_sqe * get_sqe_and_record_one();
+
+        io_uring_sqe * find_empty_sqe(io_uring ** ppuring);
+
+        void deinit_io_uring();
+
+        void init();
+
+    private :
+        std::map<int , io_context> m_fds_context;
+        base::MinHeap<base::FourWayHeap<int, std::shared_ptr<io_uring>>> m_uring;
     };
 
 }
