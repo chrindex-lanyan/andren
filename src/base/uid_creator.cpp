e8a0ae3bcf3b96b6362d9b6f5b55ba8a7d48fe7f
chrindex <7467271+chrindex@user.noreply.gitee.com>
2023-09-21 05:04:55 +0800

    添加两个生成uid的函数

---
+++ src/base/uid_creator.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "uid_creator.hh"
+
+#include <atomic>
+#include <thread>
+
+
+namespace chrindex ::andren::base
+{
+    std::atomic<uint32_t> _uid_u32_last=1000;
+    std::atomic<uint64_t> _uid_u64_last=1000;
+
+    uint32_t create_uid_u32()
+    {
+        return _uid_u32_last.fetch_add(1, std::memory_order_seq_cst);
+    }
+
+    uint64_t create_uid_u64()
+    {
+        return _uid_u64_last.fetch_add(1, std::memory_order_seq_cst);
+    }
+
+
+}
---
+++ src/base/uid_creator.hh
@@ -0,0 +1,21 @@
+﻿
+#pragma once
+
+#include <stdint.h>
+
+namespace chrindex ::andren::base
+{
+
+    /// @brief 获取一个单调自增的32位整形uid。
+    /// thread-safe : true
+    /// @return uid i32
+    uint32_t create_uid_u32();
+
+    /// @brief 获取一个单调自增的64位整形uid。
+    /// thread-safe : true
+    /// @return uid u32
+    uint64_t create_uid_u64();
+
+}
+
+
