b9455c2b5b5457024d2d9f20de668d9482c49f9d
chrindex <7467271+chrindex@user.noreply.gitee.com>
2023-09-21 05:05:18 +0800

    实现io_file的类

--- src/network/io_file.cpp
+++ src/network/io_file.cpp
@@ -1,8 +1,196 @@
 ﻿
 #include "io_file.hh"
+#include <memory>
 
 namespace chrindex::andren::network
 {
-    
+    io_file::io_file() : m_fd(-1)
+    {
+        //
+    }
+
+    io_file::io_file(std::string const & path, uint32_t flags , uint32_t mode)
+    {
+        immediately_open(path, flags, mode);
+    }
+
+    io_file::io_file(io_file && another)
+    {
+        m_fd = another.m_fd;
+        another.m_fd = -1;
+    }
+
+    io_file::~io_file()
+    {
+        immediately_close();
+    }
+
+    void io_file::async_open(std::function<void(io_file *self)> onOpen , IOService & ioservice  ,
+        std::string const & path, int dir_fd, uint32_t flags , uint32_t mode )
+    {
+        io_context context;
+        uint64_t uid = base::create_uid_u64();
+
+        context.req_context->ioData.file.dfd = dir_fd;
+        context.req_context->general.uid = uid;
+        context.req_context->general.flags = flags;
+        context.req_context->ioData.file.mode = mode;
+        memcpy(context.req_context->ioData.file.path,path.c_str(), 
+            std::min(sizeof(context.req_context->ioData.file.path), path.size())); 
+
+        context.onEvents = [onOpen = std::move(onOpen)](io_context * pcontext , int32_t cqe_res)->bool
+        {
+            io_file iofile;
+
+            iofile.resume(cqe_res);
+            if (onOpen)
+            {
+                onOpen(&iofile);
+            }
+            return true;
+        };
+        ioservice.submitRequest(uid, std::move(context));
+    }
+
+    void io_file::async_write(std::function<void (io_file *self, ssize_t wrsize)> onWrite , 
+        IOService & ioservice ,std::string data , int flags)
+    {
+        io_context context;
+        uint64_t uid = base::create_uid_u64();
+        std::string *pdata =0;
+
+        context.userdata = std::make_shared<std::string>(std::move(data));
+        pdata = reinterpret_cast<std::string*>(context.userdata.get());
+        context.req_context->general.fd = take_handle();
+        context.req_context->general.uid = uid;
+        context.req_context->general.flags = flags;
+        context.req_context->general.size = pdata->size();
+        context.req_context->ioData.bufData.buf_ptr = &(*pdata)[0];
+        
+        context.onEvents = [onWrite = std::move(onWrite)](io_context * pcontext, int32_t cqe_res)->bool
+        {
+            io_file iofile;
+            
+            iofile.resume(pcontext->req_context->general.fd);
+            if(onWrite)
+            {
+                onWrite(&iofile,cqe_res);
+            }
+            return true;
+        };
+        ioservice.submitRequest(uid, std::move(context));
+    }
+
+    void io_file::async_read(std::function<void(io_file *self, std::string && data , size_t rdsize )> onRead ,
+        IOService & ioservice , int flags)
+    {
+        io_context context;
+        uint64_t uid = base::create_uid_u64();
+
+        context.userdata = std::make_unique<std::string>();
+        context.req_context->general.uid = uid;
+        context.req_context->general.fd = take_handle();
+        context.req_context->general.flags = flags;
+
+        context.onEvents = [onRead = std::move(onRead)](io_context * pcontext, int32_t cqe_res)->bool
+        {
+            ssize_t readsize =0;
+            std::string * pstr = reinterpret_cast<std::string*>(pcontext->userdata.get());
+
+            if (cqe_res >0 && cqe_res >= sizeof(pcontext->req_context->ioData.bufData.buf))
+            {
+                pstr->append(std::string (pcontext->req_context->ioData.bufData.buf, cqe_res));
+                return false;
+            }
+
+            io_file iofile;
+
+            iofile.resume(pcontext->req_context->general.fd);
+            if (onRead)
+            {
+                readsize = pstr->size();
+                onRead(&iofile, std::move(*pstr),readsize);
+            }
+            return true;
+        };
+
+        ioservice.submitRequest(uid, std::move(context));
+    }
+
+    void io_file::async_close(std::function<void (io_file * self)> onClose , 
+        IOService & ioservice )
+    {
+        io_context context;
+        uint64_t uid = base::create_uid_u64();
+
+        context.req_context->general.uid = uid;
+        context.req_context->general.fd = take_handle();
+        context.onEvents = [onClose = std::move(onClose)](io_context * pcontex , int32_t cqe_res)->bool
+        {
+            io_file iofile;
+
+            iofile.resume(pcontex->req_context->general.fd);
+            if (onClose)
+            {
+                onClose(&iofile);
+            }
+            return true;
+        };
+        ioservice.submitRequest(uid, std::move(context));
+    }
+
+    int io_file::immediately_open(std::string const & path, uint32_t flags , uint32_t mode)
+    {
+        base::File _f ;
+        int ret = _f.open(path, flags, mode);
+        m_fd = _f.take_handle();
+        return ret;
+    }
+
+    ssize_t io_file::immediately_write(char const * data , size_t size)
+    {
+        base::File _f (m_fd);
+        ssize_t ret = _f.write(data, size);
+        _f.take_handle();
+        return ret;
+    }
+
+    ssize_t io_file::immediately_read(std::string & data)
+    {
+        base::File _f(m_fd);
+        ssize_t ret =_f.readToBuf(data);
+        _f.take_handle();
+        return ret;
+    }
+
+    void io_file::immediately_close()
+    {
+        base::File(m_fd).close();
+    }
+
+    bool io_file::valid () const 
+    {
+        return m_fd >0 ;
+    }
+
+    int io_file::take_handle()
+    {
+        int fd = m_fd;
+        m_fd = -1;
+        return fd;
+    }
+
+    int io_file::handle() const 
+    {
+        return m_fd;
+    }
+
+
+
+    void io_file::resume(int fd)
+    {
+        m_fd = fd;
+    }
+
 }
 
--- src/network/io_file.hh
+++ src/network/io_file.hh
@@ -2,9 +2,60 @@
 
 #include "../base/andren_base.hh"
 #include "io_service.hh"
+#include <any>
+#include <functional>
+#include <memory>
+#include <sys/socket.h>
+#include <utility>
 
 
 namespace chrindex::andren::network
 {
+    class io_file
+    {
+    public :
+
+        io_file();
+
+        io_file(std::string const & path, uint32_t flags , uint32_t mode = -1);
+
+        io_file(io_file && another);
+
+        ~io_file();
+
+        void async_open(std::function<void(io_file *self)> onOpen , IOService & ioservice  ,
+            std::string const & path, int dir_fd, uint32_t flags , uint32_t mode = -1);
+
+        void async_write(std::function<void (io_file *self, ssize_t wrsize)> onWrite , 
+            IOService & ioservice ,std::string data , int flags);
+
+        void async_read(std::function<void(io_file *self, std::string && data , size_t rdsize )> onRead ,
+            IOService & ioservice , int flags);
+
+        void async_close(std::function<void (io_file * self)> onClose , 
+            IOService & ioservice );
+
+        int immediately_open(std::string const & path, uint32_t flags , uint32_t mode =-1);
+
+        ssize_t immediately_write(char const * data , size_t size);
+
+        ssize_t immediately_read(std::string & data);
+
+        void immediately_close();
+
+        bool valid () const ;
+
+        int take_handle();
+
+        int handle() const ;
+
+    private :
+
+        void resume(int fd);
+
+    private :
+        int m_fd;
+    };
+
     
 }
